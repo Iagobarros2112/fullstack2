b0395c9727e156a2581632762ad2a416b74acc1f
Iagobarros2112 <iagobarrosgomes2112@gmail.com>
2024-09-24 11:34:45 -0300

    iniciada operaçoes crud

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/Iagobarros2112/fullstack/fullstack2/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/users/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}
